a0e87379c108acc79d3b6a56a6d482a274864aca
adrian121201 <adrianicalina21@gmail.com>
2024-05-09 12:04:46 +0800

    Added my project

---
+++ pages/studentlist.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-card>
+        <v-card-title>
+            Students List Page
+            <v-spacer></v-spacer>
+            <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line
+                hide-details></v-text-field>
+        </v-card-title>
+        <v-data-table :headers="headers" :items="StudentData" :search="search">
+            <template slot="item.actions" slot-scope="{item}">
+                <v-btn color="green accent-4"> DELETE</v-btn>
+                <v-btn color="red accent-4"> EDIT</v-btn>
+            </template>
+        </v-data-table>
+
+    </v-card>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            search: '',
+            headers: [
+                {
+                    text: 'Student Number',
+                    align: 'start',
+                    sortable: false,
+                    value: 'Student_no',
+                },
+                { text: 'First Name', value: 'First_name' },
+                { text: 'Last Name', value: 'Last_name' },
+                { text: 'Middle Initial', value: 'Middle_initial' },
+                { text: 'Course', value: 'Course' },
+                { text: 'Section', value: 'Section' },
+                { text: 'Year', value: 'Year' },
+                { text: '', value: "actions" }
+            ],
+            StudentData: [
+               
+
+            ],
+        }
+    },
+}
+</script>
+
+
